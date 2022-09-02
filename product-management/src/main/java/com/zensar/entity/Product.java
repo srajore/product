93da95cbd8562d491bf9167329544cc70a8bea18
SHARAD <sharad.rajore@zensar.com>
2022-09-02 09:13:34 +0530

    Basic CRUD operations

---
+++ product-management/src/main/java/com/zensar/entity/Product.java
@@ -0,0 +1,47 @@
+package com.zensar.entity;
+
+public class Product {
+	
+	private int productId;
+	private String productName;
+	private int productCost;
+	
+	
+	public Product() {
+		super();
+	}
+	public Product(int productId, String productName, int productCost) {
+		super();
+		this.productId = productId;
+		this.productName = productName;
+		this.productCost = productCost;
+	}
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public int getProductCost() {
+		return productCost;
+	}
+	public void setProductCost(int productCost) {
+		this.productCost = productCost;
+	}
+	@Override
+	public String toString() {
+		return "Product [productId=" + productId + ", productName=" + productName + ", productCost=" + productCost
+				+ "]";
+	}
+	
+	
+	
+	
+
+}
